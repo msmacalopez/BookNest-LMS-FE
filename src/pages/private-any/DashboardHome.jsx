7033465e6c94b1392608f23a5bb3a199e8716786
msmacalopez <msmacalopezs@gmail.com>
2026-01-07 23:09:19 +1100

    profile includes membership

--- src/pages/private-any/DashboardHome.jsx
+++ src/pages/private-any/DashboardHome.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function DashboardHome() {
-  return <div>DashboardHome</div>;
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">DashboardHome</h1>
+      <h1 className="text-4xl font-bold">ADD some statisticss!!!</h1>
+      <p>Different for Members and for Admins</p>
+      <p>Member - own stadistics</p>
+      <p>Admin - Library related stadistics</p>
+    </div>
+  );
 }
--- src/pages/private-any/ProfilePage.jsx
+++ src/pages/private-any/ProfilePage.jsx
@@ -12,75 +12,107 @@ export default function ProfilePage() {
   });
 
   return (
-    <div>
-      <fieldset className="fieldset bg-primary-content border-base-300 rounded-box border p-4 w-full min-h-[70vh]">
-        <legend className="fieldset-legend text-xl">Your Details</legend>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      {/* Left Side */}
+      <div>
+        <h1 className="text-primary text-2xl font-bold mb-3">Member Details</h1>
+        <hr />
+        <br />
+        <fieldset className="fieldset bg-base-300 dark:!bg-gray-600  border-base-300 rounded-box border p-4 w-full min-h-[60vh] flex flex-col items-center justify-center">
+          <label className="label text-left">Name</label>
+          <input
+            type="text"
+            className="input"
+            value={formData.fname}
+            disabled={!isEditing}
+            onChange={(e) =>
+              setFormData({ ...formData, fname: e.target.value })
+            }
+          />
+          <label className="label">Lastname</label>
+          <input
+            type="text"
+            className="input"
+            value={formData.lname}
+            disabled={!isEditing}
+            onChange={(e) =>
+              setFormData({ ...formData, lname: e.target.value })
+            }
+          />
+          <label className="label">Address</label>
+          <input
+            type="text"
+            className="input"
+            value={formData.address}
+            disabled={!isEditing}
+            onChange={(e) =>
+              setFormData({ ...formData, address: e.target.value })
+            }
+          />
+          <label className="label">Phone</label>
+          <input
+            type="text"
+            className="input"
+            value={formData.phone}
+            disabled={!isEditing}
+            onChange={(e) =>
+              setFormData({ ...formData, phone: e.target.value })
+            }
+          />
 
-        <label className="label">Email</label>
-        <input
-          type="email"
-          className="input"
-          value={formData.email}
-          disabled={true}
-          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-        />
-
-        <label className="label">Name</label>
-        <input
-          type="text"
-          className="input"
-          value={formData.fname}
-          disabled={!isEditing}
-          onChange={(e) => setFormData({ ...formData, fname: e.target.value })}
-        />
-        <label className="label">Lastname</label>
-        <input
-          type="text"
-          className="input"
-          value={formData.lname}
-          disabled={!isEditing}
-          onChange={(e) => setFormData({ ...formData, lname: e.target.value })}
-        />
-        <label className="label">Address</label>
-        <input
-          type="text"
-          className="input"
-          value={formData.address}
-          disabled={!isEditing}
-          onChange={(e) =>
-            setFormData({ ...formData, address: e.target.value })
-          }
-        />
-        <label className="label">Phone</label>
-        <input
-          type="text"
-          className="input"
-          value={formData.phone}
-          disabled={!isEditing}
-          onChange={(e) => setFormData({ ...formData, phone: e.target.value })}
-        />
-
-        <div className="mt-4 flex gap-3">
-          {!isEditing ? (
-            <button
-              className="btn btn-neutral"
-              onClick={() => setIsEditing(true)}
-            >
-              Modify details
-            </button>
-          ) : (
-            <>
-              <button className="btn btn-neutral">Save Changes</button>
+          <div className="mt-4 flex gap-3">
+            {!isEditing ? (
               <button
-                className="btn btn-ghost"
-                onClick={() => setIsEditing(false)}
+                className="btn btn-neutral"
+                onClick={() => setIsEditing(true)}
               >
-                Cancel
+                Modify details
               </button>
-            </>
-          )}
+            ) : (
+              <>
+                <button className="btn btn-neutral">Save Changes</button>
+                <button
+                  className="btn btn-ghost"
+                  onClick={() => setIsEditing(false)}
+                >
+                  Cancel
+                </button>
+              </>
+            )}
+          </div>
+        </fieldset>
+      </div>
+
+      {/* Right Side */}
+      <div>
+        <h1 className="text-primary text-2xl font-bold mb-3">My Membership</h1>
+        <hr />
+        {/* Whole TABLE */}
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Membership Number</th>
+                <th>Credentials</th>
+                <th>Starting Date</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <td>112345</td>
+                <td>Member</td>
+                <td>20/05/2025</td>
+                <td>
+                  <div className="badge badge-success">Active</div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-      </fieldset>
+      </div>
     </div>
   );
 }
